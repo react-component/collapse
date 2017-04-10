324e7b240238c279b530c937e5cd502af4854467
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-10 11:10:26 +0900

    Migrate from the deprecated React.propTypes to the prop-types package

--- src/Collapse.jsx
+++ src/Collapse.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, Children }from 'react';
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
 import CollapsePanel from './Panel';
 import openAnimationFactory from './openAnimationFactory';
 import classNames from 'classnames';
