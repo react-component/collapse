06c92a32115457cfdcec6fb05c357f782f6939b7
AhmedSarkawt <40734891+AhmedSarkawt@users.noreply.github.com>
2020-11-30 11:01:43 +0300

    Update Collapse.tsx
    Update the ternary operator to if statement.

--- src/Collapse.tsx
+++ src/Collapse.tsx
@@ -34,9 +34,11 @@ class Collapse extends React.Component<CollapseProps, CollapseState> {
     const { activeKey, defaultActiveKey } = props;
     let currentActiveKey = defaultActiveKey;
     if ('activeKey' in props) {
-      activeKey === 'unset' 
-        ? currentActiveKey = defaultActiveKey
-        : currentActiveKey = activeKey;
+      if (activeKey === 'unset) {
+        currentActiveKey = defaultActiveKey;
+    } else {
+        currentActiveKey = activeKey;
+      }
     }
 
     this.state = {
