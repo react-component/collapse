a9d2a528e1fe6f7e6c0e8d8037f71f99724194f4
狗蛋儿 <songyan_shi@163.com>
2021-09-14 10:34:41 +0800

    fix: 31039 (#215)
    * fix: 31039
    
    * test: 补充测试用例
    
    * fix: 当指定 collapsible=header 时，去除 role tabIndex

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -54,10 +54,11 @@ class CollapsePanel extends React.Component<CollapsePanelProps, any> {
     } = this.props;
 
     const disabled = collapsible === 'disabled';
+    const collapsibleHeader = collapsible === 'header';
 
     const headerCls = classNames(`${prefixCls}-header`, {
       [headerClass]: headerClass,
-      [`${prefixCls}-header-collapsible-only`]: collapsible === 'header',
+      [`${prefixCls}-header-collapsible-only`]: collapsibleHeader,
     });
     const itemCls = classNames(
       {
@@ -69,24 +70,36 @@ class CollapsePanel extends React.Component<CollapsePanelProps, any> {
     );
 
     let icon: any = <i className="arrow" />;
+
+    /** header 节点属性 */
+    const headerProps: React.HTMLAttributes<HTMLDivElement> = {
+      className: headerCls,
+      'aria-expanded': isActive,
+      onKeyPress: this.handleKeyPress,
+    };
+
     if (showArrow && typeof expandIcon === 'function') {
       icon = expandIcon(this.props);
+    }
+    if (collapsibleHeader) {
+      icon = (
+        <span style={{ cursor: 'pointer' }} onClick={() => this.handleItemClick()}>
+          {icon}
+        </span>
+      );
+    } else {
+      headerProps.onClick = this.handleItemClick;
+      headerProps.role = accordion ? 'tab' : 'button';
+      headerProps.tabIndex = disabled ? -1 : 0;
     }
 
     const ifExtraExist = extra !== null && extra !== undefined && typeof extra !== 'boolean';
 
     return (
       <div className={itemCls} style={style} id={id}>
-        <div
-          className={headerCls}
-          onClick={() => collapsible !== 'header' && this.handleItemClick()}
-          role={accordion ? 'tab' : 'button'}
-          tabIndex={disabled ? -1 : 0}
-          aria-expanded={isActive}
-          onKeyPress={this.handleKeyPress}
-        >
+        <div {...headerProps}>
           {showArrow && icon}
-          {collapsible === 'header' ? (
+          {collapsibleHeader ? (
             <span onClick={this.handleItemClick} className={`${prefixCls}-header-text`}>
               {header}
             </span>
