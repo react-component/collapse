d7539288cedcfc001394dcac2c538bacb4f65b8f
Amumu <yoyo837@hotmail.com>
2024-08-16 10:58:35 +0800

    fix vercel (#349)

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,12 +10,10 @@ export default defineConfig({
     'rc-collapse$': path.resolve('src'),
     'rc-collapse/es': path.resolve('src'),
   },
+  mfsu: false,
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'Collapse',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
-  outputPath: '.doc',
-  base: basePath,
-  publicPath,
 });
